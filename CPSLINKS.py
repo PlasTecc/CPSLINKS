fe4ee3a4c85719693c2f85503aaac92bcb0c9155
PlasTec <100198052+PlasTecc@users.noreply.github.com>
2022-02-26 16:12:19 +0300

    Update CPSLINKS.py

--- CPSLINKS.py
+++ CPSLINKS.py
@@ -5,48 +5,48 @@ import webbrowser
 import time
 import os
 
-links_json_url = "https://raw.githubusercontent.com/PlasTecc/CPSLINKS/main/links.json"
+zoom_links_json_url = "https://raw.githubusercontent.com/PlasTecc/CPSLINKS/main/links.json"
 timetable_json_url = "https://raw.githubusercontent.com/PlasTecc/CPSLINKS/main/timetable.json"
-current_date = calendar.day_name[datetime.date.today().weekday()]
-links = requests.get(links_json_url).json()
-timetable = requests.get(timetable_json_url).json()[current_date]
+current_day = calendar.day_name[datetime.date.today().weekday()]
 banner = f"""   ____ ____  ____  _     ___ _   _ _  ______  
   / ___|  _ \/ ___|| |   |_ _| \ | | |/ / ___| 
  | |   | |_) \___ \| |    | ||  \| | ' /\___ \ 
  | |___|  __/ ___) | |___ | || |\  | . \ ___) |
   \____|_|   |____/|_____|___|_| \_|_|\_\____/ 
-                                               By PlasTec#5267 | Version: 1.0 | 11A only."""
+                                               By PlasTec#5267 | Version: 2.0 | 11A only."""
 
-while True:
-    try:
-        if current_date == "Friday" or current_date == "Saturday":
-            print("There is no school today!")
-            input("Press Enter to continue...")
-            break
-        else:
-            os.system('cls' if os.name == 'nt' else 'clear')
+if current_day != "Friday" and current_day != "Saturday":
+    zoom_links = requests.get(zoom_links_json_url).json()
+    timetable = requests.get(timetable_json_url).json()[current_day]
+    while True:
+        try:
             counter = 1
+            os.system('cls' if os.name == 'nt' else 'clear')
             print(banner)
             for subject in timetable:
                 print(f"{counter}. {subject}")
                 counter += 1
-            my_choice = int(input("> "))
-            if my_choice > 0:
-                if timetable[my_choice - 1] == "GH":
-                    print("Please check the whatsapp group for the link.")
-                    input("Press Enter to continue...")
+            user_input = int(input("> "))
+            subject_index = user_input - 1
+            if user_input > 0:
+                if timetable[subject_index] != "GH":
+                    webbrowser.open(zoom_links[timetable[subject_index]])
+                    if input("Would you like to use it again? (Y/N): ").lower() == "n":
+                        break
                 else:
-                    webbrowser.open(links[timetable[my_choice - 1]])
+                    input(
+                        "Please check the whatsapp group for the link.\nPress Enter to continue...")
             else:
-                print("You have either entered letter or an invalid number!")
-            con = input("Would you like to continue? (Y/N): ").lower()
-            if con == "n":
+                if input("Invalid input!\nWould you like to try again? (Y/N): ").lower() == "n":
+                    break
+
+        except Exception:
+            if input("Invalid input!\nWould you like to try again? (Y/N): ").lower() == "n":
                 break
-    except Exception:
-        print("You have either entered letter or an invalid number!")
-        answer = input("Would you like to try again? (Y/N): ").lower()
-        if answer == "n":
-            break
+else:
+    print(banner)
+    input("There is no school today!\nPress Enter to continue...")
+
 
 print("Thanks for using CPSLINKS developed by PlasTec. Exiting in 3 seconds...")
 time.sleep(3)
